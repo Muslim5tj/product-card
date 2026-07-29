9fbdd60212d1f073900a42f20d71aa2dd5c5cd1f
Muslim5tj <mm747349@gmail.com>
2026-07-29 10:49:10 +0300

    исправил тобуляцию

--- homework-8.js
+++ homework-8.js
@@ -1,4 +1,4 @@
-const user = { 
+const user = {
   firstName: "Muslim",
   lastName: "Ibragim",
   age: 40,
@@ -7,9 +7,9 @@ const user = {
   city: "Dushanbe",
   jobStatus: "driver",
   isStudent: true,
-  skills: ["HTML", "CSS", "JS", "React", "Node.js"],
+  skills: ["HTML", "CSS", "JS", "React", "Node.js"]
 };
-// car
+
 const car = {
   brand: "KIA",
   model: "Cerato",
@@ -17,14 +17,18 @@ const car = {
   color: "yellow",
   engine: "1.6L",
   transmission: "MECHANICS",
-  mileage: 500000,
-}
- car.owner = user;
- console.log(`Владелец машины ${car.brand} ${car.model} - ${car.owner.firstName}.`);
- console.log(`Тип коробки: ${car.transmission}.`);
+  mileage: 500000
+};
 
- function checkMaxSpeed(auto) {
-  if (!('maxSpeed' in auto)) { // Используем обычные кавычки
+// Привязка владельца
+car.owner = user;
+
+console.log(`Владелец машины ${car.brand} ${car.model} — ${car.owner.firstName}.`);
+console.log(`Тип коробки: ${car.transmission}.`);
+
+// Функция проверки максимальной скорости
+function checkMaxSpeed(auto) {
+  if (!("maxSpeed" in auto)) {
     auto.maxSpeed = 195;
     console.log("Свойство maxSpeed было добавлено!");
   } else {
@@ -35,18 +39,19 @@ const car = {
 checkMaxSpeed(car);
 console.log(car);
 
-
+// Функция получения значения свойства
 function getPropertyValue(obj, key) {
-
   console.log(`Значение свойства "${key}":`, obj[key]);
 }
 
 getPropertyValue(user, "city");
 getPropertyValue(car, "model");
 
-const productNames = ["крем для лица", "Маска для волос", "Сыворотка", "Пенка для умывания", "Тоник"];
+// Массив косметических продуктов
+const productNames = ["Крем для лица", "Маска для волос", "Сыворотка", "Пенка для умывания", "Тоник"];
 console.log(productNames);
 
+// Работа с библиотекой книг
 const library = [
   { title: "Сахих Муслим", author: "Имам Муслим", genre: "Сборник хадисов" },
   { title: "Сахих аль-Бухари", author: "Имам аль-Бухари", genre: "Сборник хадисов" },
@@ -54,38 +59,28 @@ const library = [
 ];
 
 console.log("Начальный список книг:", library);
-library.push({
-  title: "40 хадисов",
-  author: "Имам ан-Навави",
-  genre: "Сборник хадисов"
-});
 
+library.push({ title: "40 хадисов", author: "Имам ан-Навави", genre: "Сборник хадисов" });
 console.log("Обновленный список книг:", library);
 
 const fantasyBooks = [
-  { title: "братство Кольца", author: "Дж. Р. Р. Толкин", genre: "Фэнтези" },
+  { title: "Братство Кольца", author: "Дж. Р. Р. Толкин", genre: "Фэнтези" },
   { title: "Две крепости", author: "Дж. Р. Р. Толкин", genre: "Фэнтези" },
   { title: "Возвращение короля", author: "Дж. Р. Р. Толкин", genre: "Фэнтези" }
 ];
 
- const allBooks = [...library, ...fantasyBooks];
+// Объединение массивов
+const allBooks = [...library, ...fantasyBooks];
+console.log("Все книги вместе после объединения:", allBooks);
 
- console.log("все книги вместе после объединения:", allBooks);
-
- function markRareBooks(booksArray) {
-
-  const updatedArray = booksArray.map(function(book) {
-
-    const bookCopy = { ...book };
-
-    if (bookCopy.genre === "Фэнтези") {
-      bookCopy.isRare = true;
-    } else {
-      bookCopy.isRare = false;
-    }
-    return bookCopy;
+// Функция маркировки редких книг
+function markRareBooks(booksArray) {
+  return booksArray.map(book => {
+    return {
+      ...book,
+      isRare: book.genre === "Фэнтези"
+    };
   });
-  return updatedArray;
 }
 
 const evaluatedLibrary = markRareBooks(allBooks);
