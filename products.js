38d5a38c3d37d5e84d8f7ebebd5152382fafad6d
Muslim5tj <mm747349@gmail.com>
2026-08-13 12:01:34 +0300

    add all product cards to the project

--- products.js
+++ products.js
@@ -15,4 +15,28 @@ export const products = [
     inStock: false,
     description: "Способствует удерживанию влаги в верхних слоях кожи"
   },
+  {
+    id: 3, 
+    title: "Гель для умывания",
+    price: 1650,
+    imgUrl: "images/gel_3.png",
+    inStock: true,
+    description: "Интенсивно очищает, не повреждает защитный барьер"
+    },
+    {
+      id: 4,
+      title: "Подарочный набор №1",
+      price: 4750,
+      imgUrl: "images/giftSet_1.png",
+      inStock: true,
+      description: "Набор, состоящий из увлажняющего крема и маски"
+    },
+    {
+      id: 5,
+      title: "Подарочный набор №5",
+      price: 7520,
+      imgUrl: "images/giftSet_5.png",
+      inStock: true,
+      description: "Весь набор средств Invisible symphony"
+    },
 ]
